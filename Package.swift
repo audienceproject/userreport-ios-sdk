26eb17f00a72b63b937b2a6f30895aba76880733
Pyry Jahkola <pyry.jahkola@sanoma.com>
2020-12-17 23:58:27 +0200

    Add support for Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "UserReport",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "UserReport",
+            targets: ["UserReportSDK"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "UserReportSDK",
+            dependencies: [],
+            path: "UserReport/UserReport",
+            exclude: ["UserReport/UserReport/Info.plist"]
+        ),
+        .testTarget(
+            name: "UserReportTests",
+            dependencies: ["UserReportSDK"],
+            path: "UserReport/Tests",
+            exclude: ["UserReport/Tests/Info.plist"]
+        )
+    ]
+)
